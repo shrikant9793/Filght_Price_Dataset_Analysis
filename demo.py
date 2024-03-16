7dfb67dfe557ec79ef1a214b874e156eda8aa596
shrikant9793 <shrinaths2@gmail.com>
2024-03-16 19:11:54 +0530

    utils added

--- demo.py
+++ demo.py
@@ -1,4 +1,14 @@
 from flight_price_dataset.logger import logging
+from flight_price_dataset.exception import flight_price_Exception
+import sys
 
-logging.info("Welcome to custome logging")
+
+#logging.info("Welcome to custome logging")
+
+try:
+    a = 1 / "10"
+    #logging.info("Welcome to custome logging")
+except Exception as e:
+    logging.info(e)
+    raise  flight_price_Exception(e, sys) from e
 
