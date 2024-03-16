3180addc73374109f59f545f8b05f7cd29904a5c
shrikant9793 <shrinaths2@gmail.com>
2024-03-16 16:52:59 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flight_price_dataset",
+    version="0.0.0",
+    author="Shrinath",
+    author_email="shrinaths2@gmail.com",
+    packages=find_packages()
+)
